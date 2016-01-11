a3e8e8ebdd4b8730f3fb7cbd4da26413f877ba64
giovanni <giovannigiampaolo58@gmail.com>
2016-01-11 19:51:38 +0100

    Add loading cirlce before main activity

--- app/src/main/java/com/doctorfinderapp/doctorfinder/MainActivity.java
+++ app/src/main/java/com/doctorfinderapp/doctorfinder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.doctorfinderapp.doctorfinder;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.AlertDialog;
+import android.app.AlertDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,4 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
