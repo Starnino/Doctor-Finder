c8b2688681328d3f5645b1cbc1625f4d836bdea4
fedebyes <bacci.1609616@studenti.uniroma1.it>
2016-01-29 12:12:44 +0100

    kinvey secrets added

--- app/src/main/java/com/doctorfinderapp/doctorfinder/SplashActivity.java
+++ app/src/main/java/com/doctorfinderapp/doctorfinder/SplashActivity.java
@@ -4,8 +4,10 @@ package com.doctorfinderapp.doctorfinder;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-
+import com.kinvey.android.Client;
+import com.kinvey.android.callback.KinveyPingCallback;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -17,6 +19,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        //roba di kinvey
+        final Client mKinveyClient = new Client.Builder(this.getApplicationContext()).build();
+        //inizializza l'oggetto della key
+        //considero splash  activity come main
+
+        // prova di ping
+        mKinveyClient.ping(new KinveyPingCallback() {
+            public void onFailure(Throwable t) {
+                Log.e(TAG, "Kinvey Ping Failed", t);
+            }
+            public void onSuccess(Boolean b) {
+                Log.d(TAG, "Kinvey Ping Success");
+            }
+        });
+
 
         TimerTask task = new TimerTask() {
             @Override
