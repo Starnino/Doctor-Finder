209b34a0e0e545cc1357d042a41cfee2e3e0e64c
fedebyes <bacci.1609616@studenti.uniroma1.it>
2016-02-07 18:52:48 +0100

    cleaned project

--- app/src/main/java/com/doctorfinderapp/doctorfinder/access/SplashActivity.java
+++ app/src/main/java/com/doctorfinderapp/doctorfinder/access/SplashActivity.java
@@ -11,17 +11,11 @@ import android.util.Pair;
 import android.widget.Toast;
 
 
-import com.doctorfinderapp.backend.myApi.MyApi;
 import com.doctorfinderapp.doctorfinder.R;
 
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -32,7 +26,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
 
-        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
@@ -59,48 +53,9 @@ public class SplashActivity extends AppCompatActivity {
 
 
 
-    //prova per app engine
 
 
-    static class EndpointsAsyncTask extends AsyncTask<android.util.Pair<Context, String>, Void, String> {
-        private static MyApi myApiService = null;
-        private Context context;
 
-        @Override
-        protected String doInBackground(android.util.Pair<Context, String>... params) {
-            if(myApiService == null) {  // Only do this once
-                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                        new AndroidJsonFactory(), null)
-                        // options for running against local devappserver
-                        // - 10.0.2.2 is localhost's IP address in Android emulator
-                        // - turn off compression when running against local devappserver
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                                abstractGoogleClientRequest.setDisableGZipContent(true);
-                            }
-                        });
-                // end options for devappserver
-
-                myApiService = builder.build();
-            }
-
-            context = params[0].first;
-            String name = params[0].second;
-
-            try {
-                return myApiService.sayHi(name).execute().getData();
-            } catch (IOException e) {
-                return e.getMessage();
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
-        }
-    }
 
 
 }
