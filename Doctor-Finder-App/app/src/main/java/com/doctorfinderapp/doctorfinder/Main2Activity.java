217e08f6ea0274d84fe0cdc1bdc7bfc0ca1a8b08
fedebyes <bacci.1609616@studenti.uniroma1.it>
2016-02-12 12:55:48 +0100

    Changes to tabbed activity (main2)

--- Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/Main2Activity.java
+++ Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.doctorfinderapp.doctorfinder;
 
 import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -17,6 +19,7 @@ import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 import com.doctorfinderapp.doctorfinder.fragment.DoctorListFragment;
 import com.doctorfinderapp.doctorfinder.fragment.DoctorMapsFragment;
@@ -80,6 +83,15 @@ public class Main2Activity extends AppCompatActivity  {
                         return true;
                     }
                 });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
     }
 
