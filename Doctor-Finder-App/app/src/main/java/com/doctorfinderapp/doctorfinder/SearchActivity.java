1aa51730afaa032d6c5bbffaab152b3fd9994bcc
fedebyes <bacci.1609616@studenti.uniroma1.it>
2016-02-16 21:32:28 +0100

    nav bar changes

--- Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/SearchActivity.java
+++ Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/SearchActivity.java
@@ -33,8 +33,8 @@ public class SearchActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
             }
         });
 
