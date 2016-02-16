42f57642736cc81170fb9e22400846944382de64
giovanni <giovannigiampaolo58@gmail.com>
2016-02-17 00:13:09 +0100

    Last version, created also userprofile with a different xml (usable) in my opinion, this active on doctor profile, is better and also usable for the users with little chenge. #qurato

--- Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/UserProfileActivity.java
+++ Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/UserProfileActivity.java
@@ -1,7 +1,28 @@
 package com.doctorfinderapp.doctorfinder;
 
-/**
- * Created by giovanni on 2/16/16.
- */
-public class UserProfileActivity {
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Utente", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
 }
