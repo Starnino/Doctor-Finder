135f5c285c3fdc65e346a6de35ec09da062d9c5f
giovanni <giovannigiampaolo58@gmail.com>
2016-02-16 22:18:45 +0100

    Prova Activity dottore...ditemi che ve ne pare

--- Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/DoctorProfileActivity.java
+++ Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/DoctorProfileActivity.java
@@ -20,7 +20,7 @@ public class DoctorProfileActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Invia una mail al pollo", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
---
+++ Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/UserProfileActivity.java
@@ -0,0 +1,7 @@
+package com.doctorfinderapp.doctorfinder;
+
+/**
+ * Created by giovanni on 2/16/16.
+ */
+public class UserProfileActivity {
+}
