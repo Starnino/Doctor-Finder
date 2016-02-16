557050327f270befa74be325b4d973c9fe843ecd
fedebyes <bacci.1609616@studenti.uniroma1.it>
2016-02-16 15:45:23 +0100

    added Doctor Profile Activity

--- Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/SearchActivity.java
+++ Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/SearchActivity.java
@@ -13,7 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Main2Activity extends AppCompatActivity
+public class SearchActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
