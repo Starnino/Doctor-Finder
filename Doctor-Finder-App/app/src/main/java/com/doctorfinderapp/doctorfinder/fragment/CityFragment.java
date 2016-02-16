ce8a17d3cb59cf19971c7cbb848ad642c05d3354
Starnino <francescostarna@hotmail.com>
2016-02-16 15:15:25 +0100

    special search activity updated

--- Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/fragment/CityFragment.java
+++ Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/fragment/CityFragment.java
@@ -1,17 +1,17 @@
 package com.doctorfinderapp.doctorfinder.fragment;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.app.ListFragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.Toast;
 
+import com.doctorfinderapp.doctorfinder.MainActivity;
 import com.doctorfinderapp.doctorfinder.R;
 import com.doctorfinderapp.doctorfinder.adapter.SpecializationAdapter;
 
@@ -19,25 +19,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- * Created by francesco on 01/02/16.
+ * Created by francesco on 02/02/16.
  */
-public class SpecialFragment extends ListFragment implements AdapterView.OnItemClickListener {
+public class CityFragment extends ListFragment implements AdapterView.OnItemClickListener{
 
     private SpecializationAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup viewGroup, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.special_fragment, viewGroup, false);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Seleziona Città");
         return view;
-
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        //use custom adapter to listview
         // /*cast array in xml to arrayList*/
         ArrayList<String> myArray = new ArrayList<>();
-        Collections.addAll(myArray, getResources().getStringArray(R.array.Specializations));
+        Collections.addAll(myArray, getResources().getStringArray(R.array.cities));
         /////////////////////////////////////
         adapter = new SpecializationAdapter(getActivity(), myArray);
         setListAdapter(adapter);
@@ -50,18 +52,10 @@ public class SpecialFragment extends ListFragment implements AdapterView.OnItemC
         icon.setImageResource(R.drawable.ic_check_circle_white_24dp);
         icon.setColorFilter(R.color.colorPrimaryDark);
         /**TODOview.startAnimation(animation);*/
-        Toast.makeText(getActivity(), adapter.getItem(position) +" selected", Toast.LENGTH_SHORT)
+        Toast.makeText(getActivity(), adapter.getItem(position) + " selected", Toast.LENGTH_SHORT)
                 .show();
-
-        Fragment fragment = new CityFragment();
-        FragmentManager fm = getFragmentManager();
-        fm.beginTransaction().replace(R.id.list_fragment, fragment).commit();
+        Intent intent = new Intent(getActivity(),
+                MainActivity.class);
+        startActivity(intent);
     }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-    }
-
 }
