cd5d861eb02a36c658d09c2e8152e6d616c6fe60
fedebyes <bacci.1609616@studenti.uniroma1.it>
2016-03-05 15:12:34 +0100

    Mario senza occhi added Occhi di mario added Bookfragment added Working on feedback fragment added

--- Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/fragment/BookFragment.java
+++ Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/fragment/BookFragment.java
@@ -10,31 +10,44 @@ import android.view.ViewGroup;
 
 import com.doctorfinderapp.doctorfinder.R;
 
-
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link FeedbackFragment.OnFragmentInteractionListener} interface
+ * {@link BookFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link FeedbackFragment#newInstance} factory method to
+ * Use the {@link BookFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FeedbackFragment extends Fragment {
-    private int index;
+public class BookFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    public FeedbackFragment() {
+    public BookFragment() {
         // Required empty public constructor
-
     }
 
-
-    public static FeedbackFragment newInstance(int index) {
-        FeedbackFragment fragment = new FeedbackFragment();
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BookFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BookFragment newInstance(String param1, String param2) {
+        BookFragment fragment = new BookFragment();
         Bundle args = new Bundle();
-        args.putInt("index", index);
-
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -42,17 +55,17 @@ public class FeedbackFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        int indexFragment = getArguments().getInt("index", 0);
-        this.index=indexFragment;
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_feedback,
-                container, false);
-        return rootView;
+        return inflater.inflate(R.layout.fragment_book, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
