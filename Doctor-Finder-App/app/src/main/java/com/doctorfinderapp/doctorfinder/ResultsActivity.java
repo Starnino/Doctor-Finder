5d917027664e2335557387fe4a273d9e047a97a2
Starnino <francescostarna@hotmail.com>
2016-03-07 02:26:08 +0100

    fab removed because were wrong good night

--- Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/ResultsActivity.java
+++ Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/ResultsActivity.java
@@ -1,6 +1,5 @@
 package com.doctorfinderapp.doctorfinder;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
@@ -29,16 +28,10 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.doctorfinderapp.doctorfinder.Class.Doctor;
 import com.doctorfinderapp.doctorfinder.Qurami.MainActivityQurami;
 import com.doctorfinderapp.doctorfinder.access.SplashActivity;
-import com.doctorfinderapp.doctorfinder.fragment.DoctorFragment;
 import com.doctorfinderapp.doctorfinder.fragment.DoctorListFragment;
 import com.doctorfinderapp.doctorfinder.fragment.DoctorMapsFragment;
-
-
-//import com.doctorfinderapp.doctorfinder.fragment.SearchFragment;
 import com.doctorfinderapp.doctorfinder.functions.GlobalVariable;
 import com.doctorfinderapp.doctorfinder.functions.RoundedImageView;
 import com.doctorfinderapp.doctorfinder.functions.Util;
@@ -48,21 +41,19 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
-
 import java.util.ArrayList;
-import java.util.BitSet;
 import java.util.List;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 
-public class ResultsActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener{
+public class ResultsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout mDrawerLayout;
     private static ViewPager viewPager;
     private static TabLayout tabs;
     private boolean isFabOpen = false;
-    private FloatingActionButton fab,fab1,fab2, fab_location;
+    private FloatingActionButton fab, fab_location;
     private Animation fab_open_normal,fab_open,fab_close,rotate_forward,rotate_backward;
 
     @Override
@@ -85,23 +76,33 @@ public class ResultsActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_results);
 
         //find fab buttons
-        fab = (FloatingActionButton)findViewById(R.id.fab);
-        fab1 = (FloatingActionButton)findViewById(R.id.fab1);
-        fab2 = (FloatingActionButton)findViewById(R.id.fab2);
-        fab_location = (FloatingActionButton)findViewById(R.id.fab_location);
+        fab_location = (FloatingActionButton) findViewById(R.id.fab_location);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
 
         //load animation
         fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
         fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
-        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
-        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
-        fab_open_normal = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_open_normal);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_backward);
+        fab_open_normal = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open_normal);
 
         //onClick button
-        fab.setOnClickListener(this);
-        fab1.setOnClickListener(this);
-        fab2.setOnClickListener(this);
-        fab_location.setOnClickListener(this);
+        fab_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DoctorMapsFragment.googleMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+                    @Override
+                    public boolean onMyLocationButtonClick() {
+                        LocationManager mgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                        if (!mgr.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                            Toast.makeText(getApplicationContext(), "GPS is disabled!", Toast.LENGTH_SHORT).show();
+                        }
+                        return false;
+                    }
+                });
+            }
+        });
 
         // Setting ViewPager for each Tabs
         viewPager = (ViewPager) findViewById(R.id.viewpager);
@@ -126,28 +127,28 @@ public class ResultsActivity extends AppCompatActivity implements View.OnClickLi
 
         //setting header
 
-        ParseUser user=ParseUser.getCurrentUser();
-        if(user!=null){
+        ParseUser user = ParseUser.getCurrentUser();
+        if (user != null) {
 
             View header = navigationView.getHeaderView(0);
-            TextView nome= (TextView) header.findViewById(R.id.name_user);
+            TextView nome = (TextView) header.findViewById(R.id.name_user);
 
             Log.d("", user.getString("fName"));
-            String name =user.getString("fName") ;
+            String name = user.getString("fName");
             nome.setText(name);
-            TextView email= (TextView) header.findViewById(R.id.email_user);
+            TextView email = (TextView) header.findViewById(R.id.email_user);
 
             email.setText(user.getEmail());
             //Re -set image
 
-            if(GlobalVariable.UserPropic!=null) {
-                Log.d("ciao",GlobalVariable.UserPropic.toString());
+            if (GlobalVariable.UserPropic != null) {
+                Log.d("ciao", GlobalVariable.UserPropic.toString());
                 RoundedImageView mImg = (RoundedImageView) header.findViewById(R.id.user_propic);
                 mImg.setImageBitmap(GlobalVariable.UserPropic);
             }
         }
-
     }
+
 
     // Add Fragments to Tabs------------------------------------------------------------------------
     private void setupViewPager(ViewPager viewPager) {
@@ -286,40 +287,9 @@ public class ResultsActivity extends AppCompatActivity implements View.OnClickLi
         return true;
     }
 
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
 
-        switch(id) {
-            case R.id.fab:
-                animateFAB();
-                break;
 
-            case R.id.fab1:
-                //TODO
-                break;
 
-            case R.id.fab2:
-                //TODO
-                break;
-
-            case R.id.fab_location:
-
-                final Context context = this;
-                DoctorMapsFragment.googleMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
-                    @Override
-                    public boolean onMyLocationButtonClick() {
-                        LocationManager mgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                        if (!mgr.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                            Toast.makeText(context, "GPS is disabled!", Toast.LENGTH_SHORT).show();
-                        }
-                        return false;
-                    }
-                });
-
-                break;
-        }
-    }
 
     static class Adapter extends FragmentPagerAdapter {
 
@@ -351,30 +321,6 @@ public class ResultsActivity extends AppCompatActivity implements View.OnClickLi
         }
     }//---------------------------------------------------------------------------------------------
 
-    //animation fab buttons
-    public void animateFAB() {
-
-        if (isFabOpen) {
-
-            fab.startAnimation(rotate_backward);
-            fab1.startAnimation(fab_close);
-            fab2.startAnimation(fab_close);
-            fab1.setClickable(false);
-            fab2.setClickable(false);
-            isFabOpen = false;
-            Log.d("button", "close");
-
-        } else {
-
-            fab.startAnimation(rotate_forward);
-            fab1.startAnimation(fab_open);
-            fab2.startAnimation(fab_open);
-            fab1.setClickable(true);
-            fab2.setClickable(true);
-            isFabOpen = true;
-            Log.d("button", "open");
-        }
-    }
 
     //switch fab
     public void switchFAB(int position){
@@ -389,15 +335,6 @@ public class ResultsActivity extends AppCompatActivity implements View.OnClickLi
 
             case 1:
                 Log.d("fab_location", "open");
-                if (isFabOpen) {
-                    fab.startAnimation(fab_close);
-                    fab1.startAnimation(fab_close);
-                    fab2.startAnimation(fab_close);
-                    fab1.setClickable(false);
-                    fab2.setClickable(false);
-                    fab.setClickable(false);
-                    isFabOpen = false;
-                }
                 fab.startAnimation(fab_close);
                 fab.setClickable(false);
                 fab_location.startAnimation(fab_open_normal);
