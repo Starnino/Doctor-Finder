aca87be33d6bdb72d258bc68d5f82b15e373bf63
giovanni <giovannigiampaolo58@gmail.com>
2016-03-07 19:49:13 +0100

    condividi added on user profile

--- Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/Intro/DefaultIntro.java
+++ Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/Intro/DefaultIntro.java
@@ -1,6 +1,6 @@
 package com.doctorfinderapp.doctorfinder.Intro;
 
-/*
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -55,4 +55,3 @@ public class DefaultIntro extends AppIntro {
     }
 
 }
-*/
