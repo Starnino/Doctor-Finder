26df6fc5f02b6f64b5fffea80b8fc0969bf27c7e
giovanni <giovannigiampaolo58@gmail.com>
2016-03-11 02:14:07 +0100

    Material color pushed into the intro

--- Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/fragment/UtenteFeedRilasciati.java
+++ Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/fragment/UtenteFeedRilasciati.java
@@ -77,7 +77,7 @@ public class UtenteFeedRilasciati extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.feedlasciati, container, false);
+        return inflater.inflate(R.layout.fragment_book, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
