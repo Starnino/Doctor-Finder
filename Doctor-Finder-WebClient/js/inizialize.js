dbd1508a4f86851b4bbe1fb9a30b6c8a9160c40f
fedebyes <bacci.1609616@studenti.uniroma1.it>
2016-03-17 22:14:16 +0100

    showing real doctors

--- Doctor-Finder-WebClient/js/inizialize.js
+++ Doctor-Finder-WebClient/js/inizialize.js
@@ -7,35 +7,75 @@
    // $('ul.tabs').tabs();
     
  
-    
-    //checkLogin();
-    
-   query();
+    var result="";
+    result+=" <div class=\"container\">\
+    <div class=\"section\"> \
+    <div class=\"card-panel\">\
+    <span class=\"blue-text text-darken-2\">Ciao!\
+    </span> ";
+     result+="</div></div>\
+     </div>";
+      var html=document.getElementById("card-view");
+      //html.innerHTML=result;
+  //console.log(html);
+  
+   query(html);
+   //console.log(query());
 });
   
-  function query() {
-      var GameScore = Parse.Object.extend("Doctor");
-    var query = new Parse.Query(GameScore);
+  function query(inner) {
+      var Doctor = Parse.Object.extend("Doctor");
+    var query = new Parse.Query(Doctor);
+     var html="";
+     html+=" <div class=\"container\">\
+    <div class=\"section\"> \
+    ";
     //query.equalTo("playerName", "Dan Stemkoski");
     query.find({
   success: function(results) {
-    alert("Successfully retrieved " + results.length + "doctors");
+    //alert("Successfully retrieved " + results.length + "doctors");
     // Do something with the returned Parse.Object values
+   
     for (var i = 0; i < results.length; i++) {
       var object = results[i];
-      //alert(object.id + ' - ' + object.get('playerName'));
+      //html+="i";
+      //console.log(html);
+      html+=createCard(object);
+      //alert(object.id + \" - \" + object.get(\"playerName'));
+      
+      
     }
+    
+     html+="</div>\</div>";
+    //console.log(html);
+    inner.innerHTML=html;
+   // console.log(inner);
+ 
+    
   },
   error: function(error) {
-   // alert("Error: " + error.code + " " + error.message);
+    alert("Error: " + error.code + " " + error.message);
   }
+  
 });
-
-       
+   
       
       
   }
   
   function createCard(Doctor){
+      var Name= Doctor.get("FirstName");
+      var result="";
+      result+="\
+    <div class=\"card-panel\">\
+    <span class=\"blue-text text-darken-2\">\
+"+Name+"</span>";
+    result+="\</div>";
+      
+      
+     // document.getElementById('card-view').innerHTML =Name;
+      //console.log(result);
+      return result;
+      
       
   }
