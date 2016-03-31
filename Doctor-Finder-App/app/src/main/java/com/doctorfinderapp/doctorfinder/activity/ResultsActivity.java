b899487820896fb177185bac70f5ec2da7478e26
giovanni <giovannigiampaolo58@gmail.com>
2016-03-31 17:08:12 +0200

    Pushalo

--- Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/activity/ResultsActivity.java
+++ Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/activity/ResultsActivity.java
@@ -26,7 +26,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageButton;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.doctorfinderapp.doctorfinder.R;
