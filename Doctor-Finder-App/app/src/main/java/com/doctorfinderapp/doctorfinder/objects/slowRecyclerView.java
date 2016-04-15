04b9e9600fd0276b01c310005bc534aca64cab92
Starnino <francescostarna@hotmail.com>
2016-04-15 13:42:07 +0200

    error manifest bug fix

--- Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/objects/slowRecyclerView.java
+++ Doctor-Finder-App/app/src/main/java/com/doctorfinderapp/doctorfinder/objects/slowRecyclerView.java
@@ -2,6 +2,7 @@ package com.doctorfinderapp.doctorfinder.objects;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
 
 /**
  * Created by francesco on 15/04/16.
@@ -15,6 +16,17 @@ public class slowRecyclerView extends RecyclerView{
         super(context);
         this.context = context;
     }
+
+    public slowRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+    }
+
+    public slowRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.context = context;
+    }
+
 
 
     @Override
